0d9969a1f3ae2947b5f5c4e76516c193834a7180
jcelisccxc <jcelis@ccxc.co>
2026-04-15 14:17:05 -0500

    Más ejercicipos practicos

--- restricciones/app.js
+++ restricciones/app.js
@@ -48,9 +48,67 @@ app.get('/tierra/:pais/:municipio', (req, res) => {
 });
 
 
+//Productos
+app.param('nombre', (req, res, next, nombre) => {
+    if (/^[a-zA-Z]+$/.test(nombre)) {
+        next();
+    } else {
+        res.status(400).send(`Error: El Nombre solo debe contener letras...`)
+    }
+});
+
+app.get('/producto/:nombre', (req, res) => {
+    res.send(`Tu producto seleccionado es: ${req.params.nombre}`); 
+});
+
+//SISTEMA MODULAR "PREFIJOS" "TIENDA"
+const tiendaRouter = express.Router();
+
+tiendaRouter.get('/catalogo', (req, res) => {
+    res.send('<h1>Catalogo</h1> <p>Producto1...</> <br> <p>Producto2...</> <p>Producto3...</> <p>Producto4...</>');
+});
+
+tiendaRouter.get('/articulo/:id', (req, res) => {
+    const {id} = req.params;
+    if (!/^\d+$/.test(id)) return res.status(400).send("ID invalido");
+    res.send(`Detalle del articulo: ${id}`);
+});
+
+app.use('/tienda', tiendaRouter);
+
+//SISTEMA CLASES
+const clasesRouter = express.Router();
+
+// Ruta: /clases/servicios
+clasesRouter.get('/servicios', (req, res) => {
+    res.send(`
+        <h1>Clases Disponibles</h1>
+        <ul>
+            <li>Mañana</li>
+            <li>Tarde</li>
+            <li>Noche</li>
+            <li>Sabatino</li>
+        </ul>
+    `);
+});
+
+// Ruta: /clases/horario/tipo
+clasesRouter.get('/horario/:tipo', (req, res) => {
+    const { tipo } = req.params;
+
+    // sólo letras
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/.test(tipo)) {
+        return res.status(400).send("<h1>Error</h1><p>Los tipos de horario deben contener solo letras.</p>");
+    }
+
+    res.send(`<h1>Consulta de Horario</h1><p>El horario a consultar es: <strong>${tipo}</strong></p>`);
+});
+
+app.use('/clases', clasesRouter);
 
 
 
+//PUERTO
 const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Servidor encendido en http://localhost:${PORT}`);
